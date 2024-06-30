eca922bdc5b5f387ca60e39092b421b91160b2ce
James <james@jamesnzl.xyz>
2024-06-30 22:00:23 +1200

    build: :arrow_up: update mediamtx

--- stream/setup.sh
+++ stream/setup.sh
@@ -6,7 +6,7 @@ mkdir mediamtx
 cd mediamtx
 
 # The version here is hard-coded for repeatability
-wget https://github.com/bluenviron/mediamtx/releases/download/v1.8.2/mediamtx_v1.8.2_linux_arm64v8.tar.gz
-tar -xvzf mediamtx_v1.8.2_linux_arm64v8.tar.gz
+wget https://github.com/bluenviron/mediamtx/releases/download/v1.8.3/mediamtx_v1.8.3_linux_arm64v8.tar.gz
+tar -xvzf mediamtx_v1.8.3_linux_arm64v8.tar.gz
 
 cp ../mediamtx.yml .
